4845c6a346798d6f18e57293e439f6473d109ba9
percy alvarez <percy.alvarez.2017@gmail.com>
2020-09-16 23:07:26 -0400

    firts commint

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name') }}</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="{{ asset('vendor/file-manager/css/file-manager.css') }}">
+    </head>
+    <body>
+    <div style="height: 600px;">
+        <div id="fm"></div>
+    </div>
+    <script src="{{ asset('vendor/file-manager/js/file-manager.js') }}"></script>
+    </body>
+</html>
